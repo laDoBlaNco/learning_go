5c558affaaa365ece29575e0dcf10f4f73fc5b59
ladoblanco <whitesidekevin@gmail.com>
2024-01-20 16:14:42 -0400

    updated functionality and refactoring of test

--- tutsEdge/the_go_path/level_1_beginner/learn_go_with_tests_GitBook/hello_world/hello_test.go
+++ tutsEdge/the_go_path/level_1_beginner/learn_go_with_tests_GitBook/hello_world/hello_test.go
@@ -2,11 +2,34 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T){
-	got :=Hello("Chris")
-	want:="Hello, Chris"
+// no with subtests
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
-	if got!=want{
-		t.Errorf("got %q wnat %q",got,want) 
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+		// We can refactor, instead of repeating the above action of checking if got and want match, we can
+		// do as Radu always says, 'when we have repeated functionality, create a function
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		// if got != want {
+		// 	t.Errorf("got %q want %q", got, want)
+		// }
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// outside of TestHello
+func assertCorrectMessage(t testing.TB, got, want string) { // testing.TB so we can run from test or benchmark
+	t.Helper() // this tells our testing suite that this is a helper func and not a test func
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
